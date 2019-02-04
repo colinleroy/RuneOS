1d8521ebe858e35ba848f9efb482dcea01aea9ac
Colin Leroy <colin@colino.net>
2019-02-04 19:59:52 +0100

    fix vars

--- 2-configure.sh
+++ 2-configure.sh
@@ -4,7 +4,9 @@ if [ "$1" = "" ]; then
 	echo "Usage: $0 [target-ip]"
 	exit 1
 fi
+ssh-keygen -f "~/.ssh/known_hosts" -R $1
+[ -f ~/.ssh/id_rsa.pub ] && ssh-copy-id -i ~/.ssh/id_rsa.pub root@$1
 
-#ssh root@$1 "which python || (pacman-key --init && pacman-key --populate archlinuxarm; pacman -S python --noconfirm)"
+ssh root@$1 "which python || (pacman-key --init && pacman-key --populate archlinuxarm && pacman -S python --noconfirm)"
 
 ansible-playbook -i hosts --ask-pass --user root runeaudio-configure.yml --extra-vars "raspberry_ip=$1"
