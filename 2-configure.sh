d9bd6eef5856baf373d1caf01ab771980d810a68
Colin Leroy <colin@colino.net>
2019-02-04 20:14:48 +0100

    Use ssh key when possible

--- 2-configure.sh
+++ 2-configure.sh
@@ -5,8 +5,18 @@ if [ "$1" = "" ]; then
 	exit 1
 fi
 ssh-keygen -f "~/.ssh/known_hosts" -R $1
-[ -f ~/.ssh/id_rsa.pub ] && ssh-copy-id -i ~/.ssh/id_rsa.pub root@$1
+if [ -f ~/.ssh/id_rsa.pub ]; then
+    ssh-copy-id -i ~/.ssh/id_rsa.pub root@$1
+else
+    sshpass=$(which sshpass 2>&1)
+    if [ "$sshpass" = "" ]; then
+        echo "Please install sshpass"
+        exit 1
+    else
+        EXTRA_ARGS="--ask-pass"
+    fi
+fi
 
 ssh root@$1 "which python || (pacman-key --init && pacman-key --populate archlinuxarm && pacman -S python --noconfirm)"
 
-ansible-playbook -i hosts --ask-pass --user root runeaudio-configure.yml --extra-vars "raspberry_ip=$1"
+ansible-playbook -i hosts $EXTRA_ARGS --user root runeaudio-configure.yml --extra-vars "raspberry_ip=$1"
