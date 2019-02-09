e425bcac1c2f7eace45e44c8f67e445ab8865309
Colin Leroy <colin@colino.net>
2019-02-09 17:01:38 +0100

    Setup an issue file at end of first phase Stop auditd spam in first phase

--- 2-configure.sh
+++ 2-configure.sh
@@ -5,6 +5,8 @@ if [ "$1" = "" ]; then
 	exit 1
 fi
 
+ssh-keygen -f ~/.ssh/known_hosts -R "$1"
+
 . ./.setup-ssh-key.sh $1
 
 ssh root@$1 "which python || (pacman-key --init && pacman-key --populate archlinuxarm && pacman -S python --noconfirm)"
