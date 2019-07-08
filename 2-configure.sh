b9bfd4b9ba0c83327dbdb8050f2275e6cffdc5b1
Colin Leroy <colin@colino.net>
2019-07-08 10:05:13 +0200

    Fixes (update pacman at the start)

--- 2-configure.sh
+++ 2-configure.sh
@@ -9,6 +9,6 @@ ssh-keygen -f ~/.ssh/known_hosts -R "$1"
 
 . ./.setup-ssh-key.sh $1
 
-ssh root@$1 "which python || (pacman-key --init && pacman-key --populate archlinuxarm && pacman -S python --noconfirm)"
+ssh root@$1 "which python || (pacman-key --init && pacman-key --populate archlinuxarm && pacman -Syy && pacman -S python --noconfirm)"
 
 ansible-playbook -i hosts $EXTRA_ARGS --user root runeaudio-configure.yml --extra-vars "raspberry_ip=$1"
